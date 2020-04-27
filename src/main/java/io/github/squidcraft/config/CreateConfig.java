2f35e52cf4a9450c9e8998063fc16cc78950b1e0
stepheninchina <60695447+stepheninchina@users.noreply.github.com>
2020-04-27 11:45:10 +0800

    删除之前的todo注释

--- src/main/java/io/github/squidcraft/config/CreateConfig.java
+++ src/main/java/io/github/squidcraft/config/CreateConfig.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.Properties;
 
 public class CreateConfig {
-    // TODO WIP update to 2020/4/26
 	/**
 	 *@author baka4n
 	 */
@@ -15,7 +14,6 @@ public class CreateConfig {
 			properties.load(new BufferedInputStream(new FileInputStream("config.sc")));
 		} catch (FileNotFoundException e) {
 			properties.put("test", "test1");
-
 			try {
 				properties.store(new BufferedOutputStream(new FileOutputStream("config.sc")), "save Config File.");
 			} catch (IOException f) {
