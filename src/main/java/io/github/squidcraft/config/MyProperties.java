1a472afd093492ae241b4f59ba184429f2419790
stepheninchina <60695447+stepheninchina@users.noreply.github.com>
2020-05-07 16:54:42 +0800

    mypropert ies.java

--- src/main/java/io/github/squidcraft/config/MyProperties.java
+++ src/main/java/io/github/squidcraft/config/MyProperties.java
@@ -8,7 +8,7 @@ import java.util.*;
  */
 public class MyProperties extends Properties {
     private static final long serialVersionUID = 1L;
-    private List<Object> keyList = new ArrayList<Object>();
+    private List<Object> keyList = new ArrayList<>();
 
     /**
      * 默认构造方法类
